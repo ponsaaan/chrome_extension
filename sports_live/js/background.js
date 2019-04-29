524047f3c8be489c6a430f7cfc96db9b4c5c45ea
yuki yamasaki <mttkd399@gmail.com>
2019-04-29 12:39:01 +0900

    jsonファイルから順番にメッセージを表示する

--- sports_live/js/background.js
+++ sports_live/js/background.js
@@ -1,6 +1,18 @@
-console.log("hello!!!!!!!!!");
+console.log("hello!!");
 
+// とりあえず静的に保存
 let msgArray = []
+var file = 'sample.json';
+var xhr = new XMLHttpRequest();
+xhr.open('GET', chrome.runtime.getURL(file), true);
+
+xhr.onload = function() {
+  if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+    msgArray = JSON.parse(xhr.responseText);
+  }
+};
+xhr.send();
+
 // scriptからリクエストが飛んできたら実行される処理
 chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
   if(request.status == "start") {
@@ -19,7 +31,14 @@ chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
 function checker() {
   var newMsg;
   if(true) {
-    newMsg="新しいメッセージです"
+    // 配列の左から順番に取り出す.
+    if(msgArray.length > 0) {
+      newMsg = msgArray[0]
+      // 一度取り出したら削除
+      msgArray.shift()
+    } else {
+      newMsg="新しいメッセージはありません"
+    }
   }
   return newMsg;
 }
